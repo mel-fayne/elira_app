3b2d3e7765bb94baa728b9dab07385fa2f069815
mel-fayne <melaniefayne33@gmail.com>
2023-05-11 01:02:05 +0300

    import API endpoints

---
+++ lib/utils/urls.dart
@@ -0,0 +1,23 @@
+const baseApiUrl = "https://melfayne.pythonanywhere.com";
+
+// ---------------- Auth Urls ----------------
+const signUpUrl = "$baseApiUrl/register";
+const signInUrl = "$baseApiUrl/login";
+const studentAccUrl = "$baseApiUrl/user_account/";
+const confirmEmailUrl = "$baseApiUrl/confirm_user/";
+const resetPassUrl = "$baseApiUrl/reset_password/";
+
+// ---------------- News Urls ----------------
+const filterNewsUrl = "$baseApiUrl/filter_news/";
+const filterJobsUrl = "$baseApiUrl/filter_jobs/";
+const filterEventsUrl = "$baseApiUrl/filter_events/";
+
+// ---------------- Student Prediction Urls ----------------
+
+const academicProfileUrl = "$baseApiUrl/academic_profile";
+const studentUnitUrl = "$baseApiUrl/student_units/";
+const techProfileUrl = "$baseApiUrl/tech_profile";
+const wxpProfileUrl = "$baseApiUrl/wx_profile";
+const wxpUrl = "$baseApiUrl/workexp";
+const ssProfileUrl = "$baseApiUrl/softskill_profile";
+const studentPredUrl = "$baseApiUrl/classifier/";
