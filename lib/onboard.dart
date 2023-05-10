811810de2a83e41e49971debdf699e11f7e27b5a
mel-fayne <melaniefayne33@gmail.com>
2023-05-11 00:38:28 +0300

    implement onboarding function

---
+++ lib/onboard.dart
@@ -0,0 +1,95 @@
+import 'package:elira_app/theme/colors.dart';
+import 'package:elira_app/theme/global_widgets.dart';
+import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+import 'package:video_player/video_player.dart';
+
+class OnBoard extends StatefulWidget {
+  const OnBoard({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _OnBoardState createState() => _OnBoardState();
+}
+
+class _OnBoardState extends State<OnBoard> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset('assets/images/Graduation.mp4');
+
+    _controller.addListener(() {
+      setState(() {});
+    });
+    _controller.setLooping(true);
+    _controller.initialize().then((_) => setState(() {}));
+    _controller.play();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_controller.value.isInitialized) {
+      return Scaffold(
+          backgroundColor: Colors.white,
+          body: SingleChildScrollView(
+              padding: const EdgeInsets.all(5),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 20, right: 20, top: 90),
+                    child: AspectRatio(
+                        aspectRatio: _controller.value.aspectRatio,
+                        child: Stack(
+                          alignment: Alignment.center,
+                          children: <Widget>[VideoPlayer(_controller)],
+                        )),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 20),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image.asset('assets/images/elira-light-logo.png',
+                            width: 100, height: 100),
+                        const Text(
+                          'Become a Better Graduate Today',
+                          style: TextStyle(
+                              color: kPriPurple,
+                              fontFamily: 'Comfortaa',
+                              fontWeight: FontWeight.w600,
+                              fontSize: 14),
+                        ),
+                      ],
+                    ),
+                  ),
+                  primaryBtn(
+                      isLoading: false,
+                      label: 'Sign In',
+                      function: () {
+                        // Get.to(const Login());
+                      }),
+                  primaryBtn(
+                      isLoading: false,
+                      label: 'Get Started',
+                      function: () {
+                        // Get.to(const SignUp());
+                      })
+                ],
+              )));
+    } else {
+      return const Center(child: CircularProgressIndicator());
+    }
+  }
+}
