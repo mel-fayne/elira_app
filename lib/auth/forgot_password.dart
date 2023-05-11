e6373fd6001ca6a827cab7d4418d54a0f7bdba0f
mel-fayne <melaniefayne33@gmail.com>
2023-05-11 03:07:36 +0300

    implement forgot password ui

---
+++ lib/auth/forgot_password.dart
@@ -0,0 +1,123 @@
+import 'package:elira_app/theme/auth_widgets.dart';
+import 'package:elira_app/theme/colors.dart';
+import 'package:elira_app/theme/global_widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class ForgotPassword extends StatefulWidget {
+  static const routeName = "/ForgotPassword";
+
+  const ForgotPassword({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return ForgotPasswordState();
+  }
+}
+
+class ForgotPasswordState extends State<ForgotPassword> {
+  bool _isLoading = false;
+  late TextEditingController emailctrl;
+  final GlobalKey<FormState> forgotpassFormKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+    emailctrl = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    emailctrl.dispose();
+    super.dispose();
+  }
+
+  void sendRecovery() {
+    final isValid = forgotpassFormKey.currentState!.validate();
+    if (!isValid) {
+      return;
+    }
+    forgotpassFormKey.currentState!.save();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: kCreamBg,
+      appBar: AppBar(
+          bottom: PreferredSize(
+              preferredSize: const Size.fromHeight(1.0),
+              child: Container(
+                color: kPriDark,
+                height: 1.0,
+              )),
+          elevation: 4,
+          toolbarHeight: 80,
+          leading: Padding(
+            padding: const EdgeInsets.only(left: 13),
+            child: IconButton(
+              icon: const Icon(Icons.keyboard_arrow_left),
+              onPressed: () {
+                Get.back();
+              },
+            ),
+          ),
+          title: const Text(
+            'Forgot Password',
+            style: TextStyle(
+                fontFamily: 'Montserrat',
+                fontSize: 18,
+                fontWeight: FontWeight.w700),
+          ),
+          centerTitle: true),
+      body: Padding(
+        padding:
+            const EdgeInsets.only(top: 30, bottom: 10, left: 25, right: 25),
+        child: Form(
+            key: forgotpassFormKey,
+            child: Column(
+              children: <Widget>[
+                const Padding(
+                    padding: EdgeInsets.only(top: 80, bottom: 25),
+                    child: Text(
+                      "Don't worry, it happens to the best of us",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: kPriPurple,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          fontFamily: 'Nunito'),
+                    )),
+                formField(
+                  label: 'Email Address',
+                  require: true,
+                  controller: emailctrl,
+                  type: TextInputType.emailAddress,
+                  validator: (value) {
+                    if (!GetUtils.isEmail(value!)) {
+                      return 'Please enter your Email';
+                    }
+                    return null;
+                  },
+                ),
+                primaryBtn(
+                  label: 'Confirm Email',
+                  isLoading: _isLoading,
+                  function: () async {
+                    setState(() {
+                      _isLoading = true;
+                    });
+                    sendRecovery();
+                    await Future.delayed(const Duration(seconds: 2));
+                    setState(() {
+                      _isLoading = false;
+                    });
+                  },
+                ),
+              ],
+            )),
+      ),
+    );
+  }
+}
